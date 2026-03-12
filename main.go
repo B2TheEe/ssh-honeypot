ddbbcad8f44d8f3a5c270bcc1c084114fadd3714
Bente Schopman <schopmanbenteb2theee@gmail.com>
2026-03-12 20:33:24 +0100

    Wachtwoorden loggen

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	"log"
-	"github.com/gliderlabs/ssh"
+    "log"
+    "github.com/gliderlabs/ssh"
 )
 
 func main() {
-	ssh.Handle(func(s ssh.Session) {
-		log.Printf("Nieuwe verbinding van %s", s.RemoteAddr())
-		s.Write([]byte("Welkom bij de SSH Honeypot! (Dit is een val.)\n"))
-		s.Exit(0)
-	})
+    // Configureer de SSH-server
+    server := &ssh.Server{
+        Addr: ":2222",
+        PasswordHandler: func(ctx ssh.Context, password string) bool {
+            log.Printf("Inlogpoging: gebruiker=%s, wachtwoord=%s, vanaf=%s",
+                ctx.User(), password, ctx.RemoteAddr())
+            // Accepteer altijd het wachtwoord voor de honeypot
+            return true
+        },
+    }
 
-	log.Println("SSH Honeypot luistert op :2222...")
-	log.Fatal(ssh.ListenAndServe(":2222", nil))
+    // Voeg een handler toe voor de sessie
+    server.Handler = func(s ssh.Session) {
+        log.Printf("Nieuwe sessie van %s", s.RemoteAddr())
+        s.Write([]byte("Welkom bij de SSH Honeypot! (Dit is een val.)\n"))
+        s.Exit(0)
+    }
+
+    // Start de server
+    log.Println("SSH Honeypot luistert op :2222...")
+    log.Fatal(server.ListenAndServe())
 }
 
